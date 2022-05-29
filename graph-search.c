3c27d3c91ccee592bea5531787534d66b98e1b28
parkswon1 <parkswon1@naver.com>
2022-05-30 00:28:10 +0900

    all

--- graph-search.c
+++ graph-search.c
@@ -114,3 +114,278 @@ int main()
 
     return 1;
 }
+
+int InitializeGraph(GraphType** h)
+{
+    int i;
+    *h = (GraphType*)malloc(sizeof(GraphType)); 
+    (*h)->vn = 0; 
+    for (i = 0; i < MAX_VTX; i++)
+    {
+        (*h)->adj_list[i].vertex = -9999;
+        (*h)->adj_list[i].link = NULL;
+    }
+    for (int x = 0; x < MAX_VTX; x++)
+    {
+        visited[x] = FALSE;
+    }
+
+    return h;
+}
+
+
+void InsertVertex(GraphType* h, int key)
+{
+    if ((h->vn) + 1 > MAX_VTX) 
+    {
+        printf("Cannot Create Vertex!\n");
+        return;
+    }
+    if (key >= MAX_VTX)
+    {
+        printf("Key Value Error!\n");
+        return;
+    }
+    for (int i = 0; i < h->vn; i++) 
+    {
+        if (h->adj_list[i].vertex == key) 
+        {
+            return;
+        }
+    }
+    h->adj_list[h->vn].vertex = key; 
+    h->adj_list[h->vn].link = NULL; 
+    h->vn = (h->vn) + 1; 
+}
+
+void InsertEdge(GraphType* h, int edgekey1, int edgekey2)
+{
+    short int a = 0;
+    int v1, v2 = 0;
+    GraphNode* node1, * node2,*cur ,*cur1;
+
+
+    for (int i = 0; i < h->vn; i++)
+    {
+        if (h->adj_list[i].vertex == edgekey1) 
+        {
+            a++;
+        }
+        if (h->adj_list[i].vertex == edgekey2) 
+        {
+            a++;
+        }
+    }
+    if (a != 2) {
+        return;
+    }
+
+    v1 = FindVertex(h, edgekey1);
+    v2 = FindVertex(h, edgekey2);
+    node1 = (GraphNode*)malloc(sizeof(GraphNode));
+    node1->link = NULL;
+    node1->vertex = edgekey1;
+    node2 = (GraphNode*)malloc(sizeof(GraphNode));
+    node2->link = NULL;
+    node2->vertex = edgekey2;
+    sort(node2, h, v1);
+    sort(node1, h, v2);
+
+    return;
+}
+void DFS_Graph(GraphType* h, int u)
+{
+    int u1 = 0;
+    u1 = FindVertex(h, u); 
+    int u2 = 0;
+
+    GraphNode* cur = NULL;
+    GraphNode* cur1 = NULL;
+    cur = h->adj_list; 
+    cur1 = cur[u1].link; 
+    visited[u1] = TRUE;
+    printf("%d", cur[u1].vertex); 
+    printf("\n");
+
+    for (cur1; cur1; cur1 = cur1->link) 
+    {
+        u2 = FindVertex(h, cur1->vertex); 
+        if (visited[u2] != TRUE) 
+            DFS_Graph(h, cur[u2].vertex); 
+    }
+
+
+}
+
+void bfs_list(GraphType* h, int v)
+{
+    int u1 = 0;
+    int u2 = 0;
+    u1 = FindVertex(h, v); 
+    GraphNode* cur, * cur1 = NULL; 
+    cur = h->adj_list;
+
+    init_queue(); 
+    visited[u1] = TRUE; 
+
+    printf("%d ", cur[u1].vertex); 
+    printf("\n");
+    enQueue(u1);
+    while (u2 != -9999)
+    {
+        u2 = deQueue(); 
+        if (u2 != -9999)  
+        {
+            for (cur1 = cur[u2].link; cur1; cur1 = cur1->link) 
+            {
+                u2 = FindVertex(h, cur1->vertex); 
+                if (visited[u2] != TRUE) 
+                {
+                    visited[u2] = TRUE; 
+                    printf("%d ", cur[u2].vertex); 
+                    printf("\n");
+                    enQueue(u2);
+                }
+
+            }
+        }
+    }
+}
+
+void init_queue()
+{
+    int x = 0;
+    rear = front = -1;
+    for (x; x < MAX_VTX; x++)
+    {
+        queue[x] = -9999;
+    }
+}
+
+void PrintGraph(GraphType* h)
+{
+    int i = 0;
+    GraphNode* cur = NULL;
+    GraphNode* cur1 = NULL;
+    if (h->adj_list[0].vertex == -9999)
+    {
+        printf("�׷����� ����ֽ��ϴ�.\n");
+        return;
+    }
+    cur = h->adj_list;
+    for (i; i < (h->vn); i++) 
+    {
+        printf("%d -> ", cur[i].vertex); 
+        cur1 = cur[i].link; 
+        while (cur1) 
+        {
+            printf("  %d  ", cur1->vertex); 
+            cur1 = cur1->link; 
+        }
+        printf("\n");
+    }
+}
+
+
+int FindVertex(GraphType* h, int edgekey)
+{
+    for (int i = 0; i < h->vn; i++) 
+    {
+        if (h->adj_list[i].vertex == edgekey) 
+        {
+            return i; 
+        }
+    }
+    return -1;
+}
+void sort(GraphNode* n1, GraphType* h, int v)
+{
+    GraphNode* cur = h->adj_list[v].link; 
+    GraphNode* stocur = NULL;
+    if (h->adj_list[v].link == NULL)
+    {
+        h->adj_list[v].link = n1; 
+        return;
+    }
+    else if (cur->vertex > n1->vertex) 
+    {
+        n1->link = cur; 
+        h->adj_list[v].link = n1; 
+        return;
+    }
+    while (cur != NULL) 
+    {
+        if (cur->vertex > n1->vertex) 
+        {
+            stocur->link = n1; 
+            n1->link = cur; 
+            return;
+
+        }
+        stocur = cur;
+        cur = cur->link; 
+
+    }
+    n1->link = cur; 
+    stocur->link = n1; 
+}
+
+void init_visited()
+{
+    int x;
+    for (x = 0; x < MAX_VTX; x++)
+    {
+        visited[x] = FALSE;
+    }
+}
+
+int deQueue()
+{
+    if (front == rear && rear == -1) 
+    {
+        printf("Queue is Empty");
+        return NULL;
+    }
+    else
+    {
+        front = front + 1; 
+        return queue[front]; 
+    }
+
+}
+
+void enQueue(int q)
+{
+    if (rear == MAX_VTX) 
+    {
+        printf("Queue is Full");
+    }
+
+    else
+    {
+        rear = rear + 1; 
+        queue[rear] = q; 
+    }
+}
+
+void freegraph(GraphType** h)
+{
+    int n;
+    GraphNode* cur = (*h)->adj_list; 
+    GraphNode* cur1 = NULL;
+    GraphNode* cur2 = NULL;
+    for (n = 0; n < MAX_VTX; n++) 
+    {
+        cur1 = cur[n].link; 
+        while (cur1 != NULL) 
+        {
+            cur2 = cur1; 
+            cur1 = cur1->link; 
+            free(cur2); 
+        }
+        free(cur1); 
+    }
+
+    free(*h); 
+    (*h) = NULL;
+}
